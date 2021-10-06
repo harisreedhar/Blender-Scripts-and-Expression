2e61c7bfecf53fa2b0fee17eddf9ea5d75badf01
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2021-10-06 13:04:20 +0530

    Update Alembic_sequence_export.py

--- Alembic_sequence_export.py
+++ Alembic_sequence_export.py
@@ -9,15 +9,15 @@ wm = bpy.context.window_manager
 wm.progress_begin(0, 100)
 total_frames = end_frame - start_frame + 1
 
-for i in range(start_frame, end_frame + 1):
+for frame in range(start_frame, end_frame + 1):
     parts = file_path.split(".")
-    _filePath = "".join(parts[:-1])+ '_' + str(i) + '.' + parts[-1]
+    _filePath = "".join(parts[:-1])+ '_' + str(frame).zfill(4) + '.' + parts[-1]
     
-    bpy.context.scene.frame_set(i)
+    bpy.context.scene.frame_set(frame)
     bpy.ops.wm.alembic_export(
         filepath = _filePath,
-        start = i,
-        end = i + 1,
+        start = frame,
+        end = frame,
         check_existing = False,
         selected = True,
         uvs = True,
@@ -28,6 +28,6 @@ for i in range(start_frame, end_frame + 1):
         curves_as_mesh = False,
         as_background_job = False,)
         
-    progress = int((i / total_frames) * 100)
+    progress = int((frame / total_frames) * 100)
     wm.progress_update(progress)
 wm.progress_end()
